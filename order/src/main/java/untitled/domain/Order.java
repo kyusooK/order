062d0ec9b30a2fb816a0d208e8ef72eb46c02637
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-09-02 16:54:10 +0900

    commit

---
+++ order/src/main/java/untitled/domain/Order.java
@@ -0,0 +1,29 @@
+package untitled.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import untitled.OrderApplication;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
+//>>> DDD / Aggregate Root
